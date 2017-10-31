a69a002741372b4cb19a6b60c66c8bec8ccc044a
Alejandro Quisbert Carvajal <alejandropqc@gmail.com>
2017-10-31 10:32:01 -0400

    Simple requests

---
+++ server.js
@@ -0,0 +1,29 @@
+const express = require('express');
+
+var app = express();
+
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', (req, res) => {
+  res.send({
+    name: 'Alejandro',
+    likes: [
+      'Biking',
+      'Countries'
+    ]
+  });
+});
+
+app.get('/about', (req, res) => {
+  res.send('About page.')
+});
+
+app.get('/bad', (req, res) => {
+  res.send({
+    errorMessage: 'Unable to handle request.'
+  });
+});
+
+app.listen(3000, () => {
+  console.log('Server is up on port 3000');
+});
